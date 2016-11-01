b4daf8a434625b026a85d99ab888654a6090db15
Alex Moundalexis <alexm@cloudera.com>
2016-11-01 11:50:53 -0400

    check_OS/check_rhel cleanup & license

--- hadoop/check_OS/check_rhel.sh
+++ hadoop/check_OS/check_rhel.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
-
+#
+# Copyright 2016 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+#
 # Look at commonly tweaked RHEL settings relevant to Hadoop.
 # Retrieval only, up to user to determine appropriateness.
+#
 
+# system and OS flags
 echo "System: $(uname -a)"
 if [ -f /etc/redhat-release ] ; then
   echo ; echo "OS: $(cat /etc/redhat-release)"
@@ -30,9 +47,7 @@ df -h
 # iptables
 echo
 if [ $RHEL7 ] ; then
-  systemctl list-unit-files --type=service | grep firewalld
-  systemctl list-unit-files --type=service | grep iptables
-  systemctl list-unit-files --type=service | grep ip6tables 
+  systemctl list-unit-files --type=service | grep -E '(firewalld|iptables|ip6tables)'
 else
   /sbin/chkconfig --list iptables
   /sbin/chkconfig --list ip6tables
